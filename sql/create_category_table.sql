fb860d60f377e635307b6d7b46df2fb1789becf7
Resident234 <gsu1234@mail.ru>
2026-08-05 19:08:25 +0300

    chore(sql): rename create_industries_table.sql to create_category_table.sql

--- sql/create_category_table.sql
+++ sql/create_category_table.sql
@@ -1,6 +1,7 @@
--- Справочник отраслей (industries)
+-- Справочник категорий (categories)
 CREATE TABLE IF NOT EXISTS category (
     code VARCHAR(255) NOT NULL,
     title VARCHAR(255) NOT NULL,
     PRIMARY KEY (code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
